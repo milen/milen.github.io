7e042ae8612940ffe1bdaea97ecbc98afb8518f8
Milen Georgiev <m.v.georgiev@gmail.com>
2020-03-23 20:46:46 +0200

    Deploy Next.js to master

--- out/_next/static/webpack/styles.e8f3d648811eefe7820e.hot-update.js
+++ out/_next/static/webpack/styles.e8f3d648811eefe7820e.hot-update.js
@@ -1,4 +1,4 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["styles"],{
+webpackHotUpdate("styles",{
 
 /***/ "./styles/styles.scss":
 /*!****************************!*\
@@ -29,7 +29,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1584978807035");
+            injectCss(link, link.href.split("?")[0] + "?unix=1584978762316");
           });
       }
     }
@@ -37,5 +37,5 @@
 
 /***/ })
 
-}]);
-//# sourceMappingURL=styles.js.map
+})
+//# sourceMappingURL=styles.e8f3d648811eefe7820e.hot-update.js.map
